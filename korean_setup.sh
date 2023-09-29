a4f7c0daee66e53230d1030f2d67d675f1f1cf29
Cardroid <number1dh@gmail.com>
2023-09-29 17:24:36 +0900

    Update Script

--- korean_setup.sh
+++ korean_setup.sh
@@ -4,5 +4,5 @@
 
 sudo apt-get install language-pack-ko -y
 sudo locale-gen ko_KR.UTF-8
-sudo dpkg-reconfigure locales
+# sudo dpkg-reconfigure locales
 sudo update-locale LANG=ko_KR.UTF-8 LC_MESSAGES=POSIX
